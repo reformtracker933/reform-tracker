ad928b49de2438dd430305b8544d7b91f523a237
khalid999devs <khalidahammeduzzal@gmail.com>
2025-10-27 02:16:46 +0600

    updated boilerplate

---
+++ src/app/(backend)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  );
+}
