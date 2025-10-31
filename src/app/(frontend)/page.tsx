590048ce27d6e261198b1396cfb1e213c6c51e66
khalid999devs <khalidahammeduzzal@gmail.com>
2025-11-01 05:22:44 +0600

    up[dated

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,18 +1,17 @@
-'use client';
+"use client";
 
-import { Hero } from '@/components/screens/Landing/Hero';
-import { Newsletter } from '@/components/screens/Landing/Newsletter';
-import { ReformNews } from '@/components/screens/Landing/ReformNews';
-import { ReformUpdate } from '@/components/screens/Landing/ReformUpdate';
+import { Hero } from "@/components/screens/Landing/Hero";
+import { Newsletter } from "@/components/screens/Landing/Newsletter";
+import { ReformNews } from "@/components/screens/Landing/ReformNews";
+import { ReformUpdate } from "@/components/screens/Landing/ReformUpdate";
 
 export default function Home() {
   return (
-    <div className='min-h-[90vh] w-full bg-background flex flex-col items-center justify-center'>
+    <div className="min-h-[90vh] w-full bg-background flex flex-col items-center justify-center">
       <Hero />
       <ReformNews />
-      <RefordmUpdateddff />
+      <ReformUpdate />
       <Newsletter />
-      {/* fs */}
     </div>
   );
 }
