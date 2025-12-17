ff371ed836319876494869d6357e946d1c53337a
khalid999devs <khalidahammeduzzal@gmail.com>
2025-12-17 23:53:27 +0600

    updated

--- src/types/translations.generated.ts
+++ src/types/translations.generated.ts
@@ -1,5 +1,5 @@
 // AUTO-GENERATED - DO NOT EDIT
-// Generated: 2025-12-17T17:51:03.538Z
+// Generated: 2025-12-17T17:53:15.746Z
 // Run 'pnpm generate:types' to regenerate
 
 export type RTLTranslations = {
